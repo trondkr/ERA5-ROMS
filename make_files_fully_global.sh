e542798ff2ccd244886d517d0f23607c96baac16
Trond Kristiansen <me@trondkristiansen.com>
2020-07-27 09:59:50 -0700

    Adding script that makes the files global (-180 to 180)

--- make_files_fully_global.sh
+++ make_files_fully_global.sh
@@ -3,6 +3,6 @@
 # The global files from ERA5 covers -180 to -179.75 which is not full global coverage in
 # ROMS. We need to add the final value to the grid which is done using CDO.
 for filename in results/*.nc; do
-		echo "test", "$filename", "halo/$(basename "$filename" .nc)_halo.nc"
-    #cdo sethalo,0,1 "$filename" "halo/$(basename "$filename" .nc)_halo.nc"
+		echo "Converting: " "$filename" " to" "halo/$(basename "$filename" .nc)_halo.nc"
+    cdo sethalo,0,1 "$filename" "halo/$(basename "$filename" .nc)_halo.nc"
 done
