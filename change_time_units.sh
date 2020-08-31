016d4572ad99d86fa5b3b57d909075a8a5099f91
Trond Kristiansen <me@trondkristiansen.com>
2020-08-31 06:38:48 -0700

    Changes filepath

--- change_time_units.sh
+++ change_time_units.sh
@@ -6,8 +6,8 @@
 # /cluster/projects/nn9630k/A20/FORCING/ERA5-ROMS/halo
 
 for filename in halo/*.nc; do
-  	echo "Converting: " "$filename" " to" "../halo_secs/$(basename "$filename")"
+  	echo "Converting: " "$filename" " to" "halo_secs/$(basename "$filename")"
 		ncatted -a units,ocean_time,o,c,"seconds since 1948-01-01" $filename tmp.nc
-		ncap2 -s ocean_time*=86400 tmp.nc "../halo_secs/$(basename "$filename")"
+		ncap2 -s ocean_time*=86400 tmp.nc "halo_secs/$(basename "$filename")"
 		rm -f tmp.nc
 done
