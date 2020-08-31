63b247161546fdf20e6c409def4d4968c5e382c6
Trond Kristiansen <me@trondkristiansen.com>
2020-08-31 06:23:43 -0700

    Added file for changing units of result files

--- change_time_units.sh
+++ change_time_units.sh
@@ -7,7 +7,7 @@
 
 for filename in halo/*.nc; do
   	echo "$(basename "$filename" .nc)"
-		echo "Converting: " "$filename" " to" "../halo_secs/"$(basename "$filename")"
+		echo "Converting: " "$filename" " to" "../halo_secs/$(basename "$filename")"
 		#ncatted -a units,ocean_time,o,c,"seconds since 1948-01-01" $filename tmp.nc
 		#ncap2 -s ocean_time*=86400 tmp.nc ../halo_secs/$filename
 done
