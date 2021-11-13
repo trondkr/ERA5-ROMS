2bb4068bc2bd2d50b10b5772fa59f247ddd1bd17
Trond Kristiansen <me@trondkristiansen.com>
2021-11-13 04:39:00 +0000

    Updated README

--- ECMWF_query.py
+++ ECMWF_query.py
@@ -18,7 +18,7 @@ class ECMWF_query:
 		# shortwave and not net shortwave to account for albedo of ice.
 
 		if not os.path.exists(self.resultsdir):
-			os.mkdir(self.resultsdir)
+			os.mkdirs(self.resultsdir)
 		if self.use_era5:
 			self.dataset = 'era5'
 			self.dataset_class = 'ea'
